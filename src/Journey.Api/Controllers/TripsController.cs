ec9436c0fe4f15fa9dbf6b7464037859e668704f
Gabriel <nonnemacher99gabriel@gmail.com>
2024-07-08 22:53:32 -0300

    Aula 1 - Criando estrutura do projeto e endpoints de CRIAR Trip e LISTAR todas as Trips

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,48 @@
+﻿using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            try
+            {
+                RegisterTripUseCase useCase = new RegisterTripUseCase();
+                
+                var response = useCase.Execute(request);
+
+                return Created(string.Empty, response);
+            }
+            catch (JourneyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ResourceErrorMessages.UNKNOWN_ERROR);
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var useCase = new GetAllTripsUseCase();
+
+            var result = useCase.Execute();
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure
+{
+    public class JourneyDbContext: DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\Gabriel\\Documents\\Github\\C#\\nlw-journey-c-sharp\\src\\Journey.Infrastructure\\DataBase\\JourneyDatabase.db");
+        }
+    }
+}
